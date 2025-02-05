a8413d05b80e9d9317a11812d7364d98c7bf0776
Esteban Soaje Ferreres <esferreres@gmail.com>
2025-02-04 21:05:31 -0300

    se borra importacion de editar asistencia

--- frontend/src/pages/ListarAsistencias.js
+++ frontend/src/pages/ListarAsistencias.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import EditarAsistencia from "./EditarAsistencia";
 import { useNavigate } from "react-router-dom";
 
 const ListarAsistencias = () => {
